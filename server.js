af1a0cc02928f65c64be473a063e79342e0a1c5d
deCkArD2077 <pungoolie888@gmail.com>
2025-09-23 22:49:08 +1000

    Slight changes before merge

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
